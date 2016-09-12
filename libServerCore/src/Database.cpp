c098382ebe9288a5a9235fbb2a02810472d38c03
worrysprite@gmail.com <worrysprite@gmail.com>
2016-09-12 17:41:27 +0800

    1、优化数据库结果集接口，增加输入输出操作符 2、数据库新增statement查询方式 3、优化数据库队列在空闲时的消耗

--- libServerCore/src/Database.cpp
+++ libServerCore/src/Database.cpp
@@ -8,123 +8,554 @@ namespace ws
 {
 	using namespace utils;
 //===================== Recordset Implements ========================
-	Recordset::Recordset(MYSQL_RES* pMysqlRes) :m_pMysqlRes(pMysqlRes)
+	Recordset::Recordset(MYSQL_RES* pMysqlRes) :mysqlRes(pMysqlRes)
 	{
-		m_NumFields = mysql_num_fields(m_pMysqlRes);
-		m_MysqlRow = NULL;
+		numFields = mysql_num_fields(mysqlRes);
+		mysqlRow = NULL;
 	}
 
 	Recordset::~Recordset()
 	{
-		mysql_free_result(m_pMysqlRes);
-		m_pMysqlRes = NULL;
+		mysql_free_result(mysqlRes);
+		mysqlRes = NULL;
 	}
 
-	bool Recordset::MoveNext()
+	bool Recordset::nextRow()
 	{
-		m_nIndex = 0x00;
-		m_MysqlRow = mysql_fetch_row(m_pMysqlRes);
-		return (m_MysqlRow != NULL);
+		fieldIndex = 0x00;
+		mysqlRow = mysql_fetch_row(mysqlRes);
+		return (mysqlRow != NULL);
 	}
 
-	bool Recordset::operator >> (unsigned long long& nValue)
+	Recordset& Recordset::operator >> (int8_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator >> (uint8_t& value)
+	{
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator >> (int16_t& value)
+	{
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator >> (uint16_t& value)
+	{
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator >> (int32_t& value)
+	{
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator >> (uint32_t& value)
+	{
+		return getInt(value);
+	}
+
+	Recordset& Recordset::operator>>(int64_t& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = atoll(szRow);
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	Recordset& Recordset::operator >> (uint64_t& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = strtoull(szRow, nullptr, 10);
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	Recordset& Recordset::operator >> (std::string& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = szRow;
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	Recordset& Recordset::operator>>(float& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = strtof(szRow, NULL);
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	Recordset& Recordset::operator>>(double& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = strtod(szRow, NULL);
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	template<typename T>
+	Recordset& Recordset::getInt(T& value)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			const char* szRow = mysqlRow[fieldIndex++];
+			if (szRow)
+			{
+				value = atoi(szRow);
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return *this;
+	}
+
+	void* Recordset::getBlob(unsigned long& datasize)
+	{
+		if (mysqlRow && fieldIndex < numFields)
+		{
+			MYSQL_FIELD* field = mysql_fetch_field_direct(mysqlRes, fieldIndex);
+			if (IS_BLOB(field->type))
+			{
+				datasize = field->max_length;
+				if (datasize > 0)
+				{
+					const char* szRow = mysqlRow[fieldIndex++];
+					void* data = malloc(datasize);
+					memcpy(data, szRow, datasize);
+					return data;
+				}
+			}
+		}
+		else
+		{
+			Log::e("mysql fetch field out of range!");
+		}
+		return nullptr;
+	}
+
+//===================== MysqlStatement Implements ========================
+
+	DBStatement::DBStatement(const char* sql, MYSQL_STMT* mysql_stmt) :
+		_strSQL(sql), stmt(mysql_stmt), paramBind(nullptr), paramIndex(0), _numParams(0), 
+		_numResultFields(0), resultBind(nullptr), resultMetadata(nullptr), _numRows(0), _lastInsertId(0)
+	{
+		// bind params
+		_numParams = mysql_stmt_param_count(stmt);
+		if (_numParams > 0)
+		{
+			paramBind = new MYSQL_BIND[_numParams];
+			memset(paramBind, 0, sizeof(MYSQL_BIND)* _numParams);
+		}
+
+		// bind result
+		resultMetadata = mysql_stmt_result_metadata(stmt);
+		if (resultMetadata)
+		{
+			_numResultFields = mysql_num_fields(resultMetadata);
+			resultBind = new MYSQL_BIND[_numResultFields];
+			memset(resultBind, 0, sizeof(MYSQL_BIND)* _numResultFields);
+			MYSQL_FIELD* fields = mysql_fetch_fields(resultMetadata);
+			for (int i = 0; i < _numResultFields; ++i)
+			{
+				resultBind[i].buffer_type = fields[i].type;
+				unsigned long buffer_length(0);
+				switch (fields[i].type)
+				{
+				case MYSQL_TYPE_TINY:
+					buffer_length = 1;
+					break;
+				case MYSQL_TYPE_SHORT:
+					buffer_length = 2;
+					break;
+				case MYSQL_TYPE_LONG:
+					buffer_length = 4;
+					break;
+				case MYSQL_TYPE_LONGLONG:
+					buffer_length = 8;
+					break;
+				case MYSQL_TYPE_FLOAT:
+					buffer_length = 4;
+					break;
+				case MYSQL_TYPE_DOUBLE:
+					buffer_length = 8;
+					break;
+				case MYSQL_TYPE_TIME:
+				case MYSQL_TYPE_DATE:
+				case MYSQL_TYPE_DATETIME:
+					buffer_length = sizeof(MYSQL_TIME);
+					break;
+				default:
+					buffer_length = fields[i].length;
+					break;
+				}
+				resultBind[i].buffer = malloc(buffer_length);
+				resultBind[i].buffer_length = buffer_length;
+				resultBind[i].is_null = new my_bool(0);
+				resultBind[i].length = new unsigned long(0);
+			}
+			if (mysql_stmt_bind_result(stmt, resultBind))
+			{
+				Log::e("mysql bind result failed: %s", mysql_stmt_error(stmt));
+			}
+		}
+	}
+
+	DBStatement::~DBStatement() 
+	{
+		if (paramBind)
+		{
+			delete[] paramBind;
+			paramBind = nullptr;
+		}
+		if (resultBind)
+		{
+			for (int i = 0; i < _numResultFields; ++i)
+			{
+				free(resultBind[i].buffer);
+				delete resultBind[i].is_null;
+				delete resultBind[i].length;
+			}
+			delete[] resultBind;
+			resultBind = nullptr;
+		}
+		mysql_free_result(resultMetadata);
+		if (stmt)
+		{
+			mysql_stmt_close(stmt);
+			stmt = nullptr;
+		}
+	}
+
+	template<typename T>
+	DBStatement& ws::DBStatement::bindNumberParam(T& value, enum_field_types type, bool isUnsigned)
+	{
+		if (paramIndex < _numParams)
+		{
+			MYSQL_BIND& b = paramBind[paramIndex++];
+			b.buffer_type = type;
+			b.buffer = &value;
+			b.is_unsigned = isUnsigned;
+		}
+		else
+		{
+			Log::e("mysql bind params out of range!");
+		}
+		return *this;
+	}
+
+	DBStatement& DBStatement::operator<<(int8_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_TINY, false);
+	}
+
+	DBStatement& DBStatement::operator<<(uint8_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_TINY, true);
+	}
+
+	DBStatement& DBStatement::operator<<(int16_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_SHORT, false);
+	}
+
+	DBStatement& DBStatement::operator<<(uint16_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_SHORT, true);
+	}
+
+	DBStatement& DBStatement::operator<<(int32_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_LONG, false);
+	}
+
+	DBStatement& DBStatement::operator<<(uint32_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_LONG, true);
+	}
+
+	DBStatement& DBStatement::operator<<(int64_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_LONGLONG, false);
+	}
+
+	DBStatement& DBStatement::operator<<(uint64_t& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_LONGLONG, true);
+	}
+
+	DBStatement& DBStatement::operator<<(float& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_FLOAT, false);
+	}
+
+	DBStatement& DBStatement::operator<<(double& value)
+	{
+		return bindNumberParam(value, MYSQL_TYPE_DOUBLE, false);
+	}
+
+	DBStatement& DBStatement::bindString(const char* value, unsigned long* length)
+	{
+		if (paramIndex < _numParams)
+		{
+			MYSQL_BIND& b = paramBind[paramIndex];
+			b.buffer_type = MYSQL_TYPE_VAR_STRING;
+			b.buffer = (void*)value;
+			b.buffer_length = *length;
+			b.length = length;
+			++paramIndex;
+		}
+		else
+		{
+			Log::e("mysql bind params out of range!");
+		}
+		return *this;
+	}
+
+	void DBStatement::bindBlob(enum_field_types type, void* data, unsigned long* size)
+	{
+		if (paramIndex < _numParams)
+		{
+			MYSQL_BIND& b = paramBind[paramIndex];
+			b.buffer_type = type;
+			b.buffer = data;
+			b.buffer_length = *size;
+			b.length = size;
+			++paramIndex;
+		}
+		else
+		{
+			Log::e("mysql bind params out of range!");
+		}
+	}
+
+	bool DBStatement::execute()
+	{
+		_numRows = 0;
+		_lastInsertId = 0;
+		if (_numParams > 0)
+		{
+			if (mysql_stmt_bind_param(stmt, paramBind))
+			{
+				Log::e("mysql bind params error");
+				return false;
+			}
+		}
+		if (mysql_stmt_execute(stmt))
+		{
+			Log::e("mysql execute failed: %s, sql=%s", mysql_stmt_error(stmt), strSQL());
 			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = strtoull(szRow, (char**)(szRow + strlen(szRow)), 10);
+		}
+		if (resultMetadata)
+		{
+			if (mysql_stmt_store_result(stmt))
+			{
+				Log::e("mysql store result failed: %s", mysql_stmt_error(stmt));
+				return false;
+			}
+		}
+		_numRows = mysql_stmt_affected_rows(stmt);
+		_lastInsertId = mysql_stmt_insert_id(stmt);
 		return true;
 	}
 
-	bool Recordset::operator >> (unsigned int& nValue)
+	bool DBStatement::nextRow()
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
+		resultIndex = 0;
+		int code = mysql_stmt_fetch(stmt);
+		switch (code)
+		{
+		case 0:
+			return true;
+		case MYSQL_NO_DATA:
 			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		case MYSQL_DATA_TRUNCATED:
+			Log::w("mysql fetch truncated!");
+			return true;
+		default:
+			Log::e("mysql fetch error: %s", mysql_stmt_error(stmt));
+			return false;
+		}
 	}
 
-	bool Recordset::operator >> (unsigned short& nValue)
+	void DBStatement::reset()
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		memset(paramBind, 0, sizeof(MYSQL_BIND)* _numParams);
+		for (int i = 0; i < _numResultFields; ++i)
+		{
+			memset(resultBind[i].buffer, 0, resultBind[i].buffer_length);
+			*resultBind[i].is_null = 0;
+			*resultBind[i].length = 0;
+		}
+		paramIndex = 0;
+		resultIndex = 0;
 	}
 
-	bool Recordset::operator >> (int& nValue)
+	template<typename T>
+	DBStatement& ws::DBStatement::getNumberValue(T& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		if (resultIndex < _numResultFields)
+		{
+			if (IS_NUM(resultBind[resultIndex].buffer_type))
+			{
+				if (*resultBind[resultIndex].is_null)
+				{
+					value = 0;
+				}
+				else
+				{
+					value = *(T*)resultBind[resultIndex].buffer;
+				}
+			}
+			++resultIndex;
+		}
+		else
+		{
+			Log::e("mysql get result out of range!");
+		}
+		return *this;
 	}
 
-	bool Recordset::operator >> (short& nValue)
+	DBStatement& DBStatement::operator>>(int8_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		return getNumberValue(value);
 	}
 
-	bool Recordset::operator >> (unsigned char& nValue)
+	DBStatement& DBStatement::operator>>(uint8_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		return getNumberValue(value);
 	}
 
-	bool Recordset::operator >> (char& nValue)
+	DBStatement& DBStatement::operator>>(int16_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		nValue = atoi(szRow);
-		return true;
+		return getNumberValue(value);
 	}
 
-	bool Recordset::operator >> (std::string& strValue)
+	DBStatement& DBStatement::operator>>(uint16_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		strValue = szRow;
-		return true;
+		return getNumberValue(value);
 	}
 
-	bool Recordset::operator>>(float& value)
+	DBStatement& DBStatement::operator>>(int32_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		value = strtof(szRow, NULL);
-		return true;
+		return getNumberValue(value);
 	}
 
-	bool Recordset::operator>>(double& value)
+	DBStatement& DBStatement::operator>>(uint32_t& value)
 	{
-		if ((m_MysqlRow == NULL) || (m_nIndex >= m_NumFields))
-			return false;
-		const char* szRow = m_MysqlRow[m_nIndex++];
-		if (szRow == NULL) return false;
-		value = strtod(szRow, NULL);
-		return true;
+		return getNumberValue(value);
+	}
+
+	DBStatement& DBStatement::operator>>(int64_t& value)
+	{
+		return getNumberValue(value);
+	}
+
+	DBStatement& DBStatement::operator>>(uint64_t& value)
+	{
+		return getNumberValue(value);
+	}
+
+	DBStatement& DBStatement::operator>>(float& value)
+	{
+		return getNumberValue(value);
+	}
+
+	DBStatement& DBStatement::operator>>(double& value)
+	{
+		return getNumberValue(value);
+	}
+
+	DBStatement& DBStatement::operator>>(std::string& value)
+	{
+		if (resultIndex < _numResultFields)
+		{
+			if (*resultBind[resultIndex].is_null)
+			{
+				value = "";
+			}
+			else
+			{
+				value = std::string((char*)resultBind[resultIndex].buffer, *resultBind[resultIndex].length);
+			}
+			++resultIndex;
+		}
+		else
+		{
+			Log::e("mysql get result out of range!");
+		}
+		return *this;
+	}
+
+	void* DBStatement::getBlob(unsigned long& datasize)
+	{
+		datasize = *resultBind[resultIndex].length;
+		if (resultIndex < _numResultFields)
+		{
+			void* data(nullptr);
+			if (!(*resultBind[resultIndex].is_null) && datasize > 0)
+			{
+				data = malloc(datasize);
+				memcpy(data, resultBind[resultIndex].buffer, datasize);
+			}
+			++resultIndex;
+			return data;
+		}
+		else
+		{
+			Log::e("mysql get result out of range!");
+			return nullptr;
+		}
 	}
 
 //===================== Database Implements ========================
@@ -145,12 +576,11 @@ namespace ws
 
 	bool Database::logon()
 	{
-		static std::mutex s_dbInitLock;
-		s_dbInitLock.lock();
+		static std::mutex initMtx;
+		initMtx.lock();
 		logoff();
 		mysql = mysql_init(NULL);
-		s_dbInitLock.unlock();
-
+		initMtx.unlock();
 		if (mysql == NULL) return false;
 
 		MYSQL* pMysql = mysql_real_connect(mysql, dbConfig.strHost.c_str(),
@@ -159,7 +589,7 @@ namespace ws
 
 		if (pMysql)
 		{
-			mysql_set_character_set(pMysql, "utf8");
+			mysql_set_character_set(pMysql, "utf8mb4");
 			Log::i("mysql connect successful.");
 		}
 		else
@@ -175,13 +605,17 @@ namespace ws
 		{
 			mysql_close(mysql);
 			mysql = NULL;
-		}
-	}
 
-	bool Database::isConnected()
-	{
-		if (mysql == NULL) return false;
-		return (mysql_ping(mysql) == 0);
+			for (auto &cache : stmtCache)
+			{
+				for (auto stmt : *cache.second)
+				{
+					delete stmt;
+				}
+				delete cache.second;
+			}
+			stmtCache.clear();
+		}
 	}
 
 	std::shared_ptr<Recordset> Database::query(const char* strSQL, int nCommit /*= 1*/)
@@ -197,12 +631,18 @@ namespace ws
 			if (pMysqlRes != NULL)
 			{
 				numResultRows = mysql_num_rows(pMysqlRes);
-				if (numResultRows > 0x00)
+				if (numResultRows > 0)
 				{
 					pRecordset = new Recordset(pMysqlRes);
 				}
-				else if (pMysqlRes != NULL) mysql_free_result(pMysqlRes);
-				if (nCommit) mysql_commit(mysql);
+				else
+				{
+					mysql_free_result(pMysqlRes);
+				}
+				if (nCommit)
+				{
+					mysql_commit(mysql);
+				}
 			}
 		}
 		else
@@ -218,10 +658,53 @@ namespace ws
 		return std::shared_ptr<Recordset>(pRecordset);
 	}
 
-	my_ulonglong Database::getInsertId()
+	DBStatement* Database::prepare(const char* strSQL)
 	{
-		if (mysql == NULL) return 0;
-		return mysql_insert_id(mysql);
+		auto iter = stmtCache.find(std::string(strSQL));
+		DBStatement* dbStmt(nullptr);
+		if (iter != stmtCache.end())
+		{
+			auto pool = iter->second;
+			if (pool->size())
+			{
+				dbStmt = pool->back();
+				pool->pop_back();
+			}
+		}
+		if (dbStmt)
+		{
+			return dbStmt;
+		}
+		MYSQL_STMT* stmt = mysql_stmt_init(mysql);
+		if (0 != mysql_stmt_prepare(stmt, strSQL, (unsigned long)strlen(strSQL)))
+		{
+			Log::e("mysql prepare error, sql=%s, error=%s", strSQL, mysql_stmt_error(stmt));
+			mysql_stmt_close(stmt);
+			return NULL;
+		}
+		return new DBStatement(strSQL, stmt);
+	}
+
+	void Database::release(DBStatement* dbStmt)
+	{
+		if (!dbStmt)
+		{
+			return;
+		}
+		std::string sql(dbStmt->strSQL());
+		dbStmt->reset();
+		auto iter = stmtCache.find(sql);
+		StmtPool* pool(nullptr);
+		if (iter == stmtCache.end())
+		{
+			pool = new StmtPool;
+			stmtCache.insert(std::make_pair(sql, pool));
+		}
+		else
+		{
+			pool = iter->second;
+		}
+		pool->push_back(dbStmt);
 	}
 
 //===================== DBRequestQueue Implements ========================
@@ -232,12 +715,17 @@ namespace ws
 
 	DBQueue::~DBQueue()
 	{
-		isExit = true;
+		while (workQueueLength > 0)
+		{
+			Log::d("DBQueue waiting for db requests complete, remaining: %d", workQueueLength);
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		this->isExit = true;
 		for (auto th : workerThreads)
 		{
 			th->join();
 			delete th;
-			Log::d("db worker thread joined.");
+			Log::d("DBQueue worker thread joined.");
 		}
 	}
 
@@ -275,7 +763,7 @@ namespace ws
 		}
 	}
 
-	DBQueue::PtrDBRequest DBQueue::getRequest()
+	PtrDBRequest DBQueue::getRequest()
 	{
 		PtrDBRequest request(nullptr);
 		workMtx.lock();
@@ -300,22 +788,25 @@ namespace ws
 	{
 		Database db;
 		db.setDBConfig(config);
+		PtrDBRequest request;
+		const std::chrono::milliseconds requestWait(50);
+		const std::chrono::microseconds connectWait(500);
 		while (!this->isExit)
 		{
+			if (!request && !(request = getRequest()))
+			{
+				std::this_thread::sleep_for(requestWait);
+				continue;
+			}
 			while (!db.isConnected())
 			{
 				db.logon();
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				continue;
-			}
-			PtrDBRequest request(getRequest());
-			if (!request)
-			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
-				continue;
+				std::this_thread::sleep_for(connectWait);
 			}
 			request->onRequest(db);
 			finishRequest(request);
+			request.reset();
 		}
 	}
+
 }
--- libServerCore/src/Database.h
+++ libServerCore/src/Database.h
@@ -4,6 +4,7 @@
 #include <mysql.h>
 #include <string>
 #include <list>
+#include <map>
 #include <mutex>
 #include <vector>
 #include <thread>
@@ -11,6 +12,8 @@
 
 namespace ws
 {
+	class Database;
+
 	struct MYSQL_CONFIG
 	{
 		unsigned int nPort;
@@ -21,55 +24,99 @@ namespace ws
 		std::string strUnixSock;
 	};
 
+	class DBStatement
+	{
+		friend class Database;
+	public:
+		DBStatement(const char* sql, MYSQL_STMT* mysql_stmt);
+		~DBStatement();
+
+		template<typename T>
+		DBStatement& bindNumberParam(T& value, enum_field_types type, bool isUnsigned);
+		DBStatement& operator<<(int8_t& value);
+		DBStatement& operator<<(uint8_t& value);
+		DBStatement& operator<<(int16_t& value);
+		DBStatement& operator<<(uint16_t& value);
+		DBStatement& operator<<(int32_t& value);
+		DBStatement& operator<<(uint32_t& value);
+		DBStatement& operator<<(int64_t& value);
+		DBStatement& operator<<(uint64_t& value);
+		DBStatement& operator<<(float& value);
+		DBStatement& operator<<(double& value);
+		DBStatement& bindString(const char* value, unsigned long* length);
+		void bindBlob(enum_field_types type, void* data, unsigned long* size);
+
+		template<typename T>
+		DBStatement& getNumberValue(T& value);
+		DBStatement& operator>>(int8_t& value);
+		DBStatement& operator>>(uint8_t& value);
+		DBStatement& operator>>(int16_t& value);
+		DBStatement& operator>>(uint16_t& value);
+		DBStatement& operator>>(int32_t& value);
+		DBStatement& operator>>(uint32_t& value);
+		DBStatement& operator>>(int64_t& value);
+		DBStatement& operator>>(uint64_t& value);
+		DBStatement& operator>>(float& value);
+		DBStatement& operator>>(double& value);
+		DBStatement& operator>>(std::string& value);
+
+		void* getBlob(unsigned long& datasize);
+
+		bool execute();
+		bool nextRow();
+		void reset();
+
+		inline void skipFields(int num){ resultIndex += num; }
+		inline int numParams(){ return _numParams; }
+		inline int numResultFields(){ return _numResultFields; }
+		inline my_ulonglong numRows(){ return _numRows; }
+		inline my_ulonglong lastInsertId(){ return _lastInsertId; }
+		inline const char* strSQL(){ return _strSQL.c_str(); }
+
+	private:
+		int				paramIndex;
+		int				_numParams;
+		int				resultIndex;
+		int				_numResultFields;
+		my_ulonglong	_numRows;
+		my_ulonglong	_lastInsertId;
+		std::string		_strSQL;
+		MYSQL_STMT*		stmt;
+		MYSQL_BIND*		paramBind;
+		MYSQL_BIND*		resultBind;
+		MYSQL_RES*		resultMetadata;
+	};
+
 	class Recordset
 	{
 	public:
 		Recordset(MYSQL_RES* pMysqlRes);
 		virtual ~Recordset();
 
-		bool MoveNext();
-		bool operator >> (unsigned long long& nValue);
-		bool operator >> (unsigned int& nValue);
-		bool operator >> (unsigned short& nValue);
-		bool operator >> (int& nValue);
-		bool operator >> (short& nValue);
-		bool operator >> (unsigned char& nValue);
-		bool operator >> (char& nValue);
-		bool operator >> (std::string& strValue);
-		bool operator >> (float& value);
-		bool operator >> (double& value);
+		bool nextRow();
+		Recordset& operator >> (int8_t& value);
+		Recordset& operator >> (uint8_t& value);
+		Recordset& operator >> (int16_t& value);
+		Recordset& operator >> (uint16_t& value);
+		Recordset& operator >> (int32_t& value);
+		Recordset& operator >> (uint32_t& value);
+		Recordset& operator >> (int64_t& value);
+		Recordset& operator >> (uint64_t& value);
+		Recordset& operator >> (float& value);
+		Recordset& operator >> (double& value);
+		Recordset& operator >> (std::string& value);
+		void*				getBlob(unsigned long& datasize);
 
 	protected:
-		unsigned int       m_nIndex;
-		unsigned int       m_NumFields;
-		MYSQL_ROW  m_MysqlRow;
-		MYSQL_RES* m_pMysqlRes;
+		unsigned int		fieldIndex;
+		unsigned int		numFields;
+		MYSQL_ROW			mysqlRow;
+		MYSQL_RES*			mysqlRes;
+
+		template<typename T>
+		Recordset&			getInt(T& value);
 	};
-
-	class Database
-	{
-	public:
-		Database();
-		virtual ~Database();
-
-		void setDBConfig(const MYSQL_CONFIG& config);
-
-		bool logon();
-		void logoff();
-		bool isConnected();
-		inline my_ulonglong getAffectedRows() { return numAffectedRows; };
-		inline my_ulonglong getResultRows() { return numResultRows; };
-		std::shared_ptr<Recordset> query(const char* strSQL, int nCommit = 1);
-		my_ulonglong getInsertId();
-
-	protected:
-		MYSQL_CONFIG dbConfig;
-		MYSQL* mysql;
-		my_ulonglong numAffectedRows;
-		my_ulonglong numResultRows;
-
-	private:
-	};
+	typedef std::shared_ptr<Recordset> PtrDBRecord;
 
 	class DBRequest
 	{
@@ -78,12 +125,47 @@ namespace ws
 		virtual void onRequest(Database& db) = 0;
 		virtual void onFinish() = 0;
 	};
+	typedef std::shared_ptr<DBRequest> PtrDBRequest;
+
+	class Database
+	{
+	public:
+		Database();
+		virtual ~Database();
+
+		void setDBConfig(const MYSQL_CONFIG& config);
+		bool logon();
+		void logoff();
+
+		inline bool isConnected()
+		{
+			if (mysql == NULL) return false;
+			return (mysql_ping(mysql) == 0);
+		}
+		inline my_ulonglong				getAffectedRows() { return numAffectedRows; };
+		inline my_ulonglong				getResultRows() { return numResultRows; };
+		inline my_ulonglong				getInsertId()
+		{
+			if (mysql == NULL) return 0;
+			return mysql_insert_id(mysql);
+		}
+
+		DBStatement*					prepare(const char* strSQL);
+		void							release(DBStatement* dbStmt);
+		PtrDBRecord						query(const char* strSQL, int nCommit = 1);
+		
+	protected:
+		MYSQL_CONFIG						dbConfig;
+		MYSQL*								mysql;
+		my_ulonglong						numAffectedRows;
+		my_ulonglong						numResultRows;
+		typedef std::list<DBStatement*>		StmtPool;
+		std::map<std::string, StmtPool*>	stmtCache;
+	};
 
 	class DBQueue
 	{
 	public:
-		typedef std::shared_ptr<DBRequest> PtrDBRequest;
-
 		DBQueue();
 		virtual ~DBQueue();
 
@@ -98,14 +180,14 @@ namespace ws
 		void				finishRequest(PtrDBRequest request);
 		void				DBWorkThread();
 
-		MYSQL_CONFIG		config;
-		std::mutex			workMtx;
-		DBRequestList		workQueue;
-		size_t				workQueueLength;
-		std::mutex			finishMtx;
-		DBRequestList		finishQueue;
-		bool				isExit;
-		std::vector<std::thread*> workerThreads;
+		MYSQL_CONFIG				config;
+		std::mutex					workMtx;
+		DBRequestList				workQueue;
+		size_t						workQueueLength;
+		std::mutex					finishMtx;
+		DBRequestList				finishQueue;
+		bool						isExit;
+		std::vector<std::thread*>	workerThreads;
 	};
 
 }
